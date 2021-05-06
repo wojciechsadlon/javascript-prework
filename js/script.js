bfcca546c522adbb7ba0d112702dd69600d0cbc5
Wojciech Sadłoń <wojciechsadlon@gmail.com>
2021-05-06 19:13:36 +0200

    Add game logic to script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem' + computerMove +'! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-// w sumie nie wiem czy na tym polegalo to pierwsze zadanie ;)
+let computerMove = 'nieznany ruch';
 
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else if (randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2'){
+    playerMove = 'papier';
+} else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
