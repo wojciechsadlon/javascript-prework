be9c685f7a95fbbc943a67d4c913d4cb874590ae
Wojciech Sadłoń <wojciechsadlon@gmail.com>
2021-05-06 18:27:37 +0200

    Add content to script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = 'kamień';
+let playerMove = 'papier';
+
+printMessage('Zagrałem' + computerMove +'! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+
+// w sumie nie wiem czy na tym polegalo to pierwsze zadanie ;)
+
